9d08fc9f27ae15bed457772367e49b34048071f9
ellynmichelle <63127351+ellynmichelle@users.noreply.github.com>
2020-05-02 12:11:14 -0700

    Set up Google Fonts and did minimal font styling
    Set up Google Fonts link to use Jost and Nunito. Changed style.less to use these fonts with a little bit of styling for H1 and H2.

--- includes/header-alt.inc.html.php
+++ includes/header-alt.inc.html.php
@@ -16,6 +16,12 @@ Programmer: Ellyn Culmsee
 
     <meta charset="utf-8">
 
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Jost%7CNunito&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
     <link href="../styles/reset.css" rel="stylesheet">
     <link href="../styles/helper.css" rel="stylesheet">
     <link href="../styles/grid.css" rel="stylesheet">
